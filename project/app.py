fcf471d6b8fa4d926bd40fce7746631f6b546785
Karan <karanbhatia@protonmail.com>
2022-03-06 10:39:18 +0530

    Commit #4: Displaying Total Scores

--- project/app.py
+++ project/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html', game_id = 111)
+	return render_template('index.html')
 
 
 @app.route("/submit", methods=["GET","POST"])
@@ -13,10 +13,10 @@ def submit():
 	if request.method == 'POST':
 		req = request.form
 		print(request.form)
+
+
+		total1 = int(req.get("tr1"))+int(req.get("award1"))+int(req.get("milestone1"))+int(req.get("gameboard1"))+int(req.get("cards1"))
+		total2 = int(req.get("tr2"))+int(req.get("award2"))+int(req.get("milestone2"))+int(req.get("gameboard2"))+int(req.get("cards2"))
+		total3 = int(req.get("tr3"))+int(req.get("award3"))+int(req.get("milestone3"))+int(req.get("gameboard3"))+int(req.get("cards3"))
 		
-
-		total1 = req.get("tr1")+req.get("award1")+req.get("milestone1")+req.get("gameboard1")+req.get("cards1")
-		print(total1)
-		return redirect(request.url)
-
-	return render_template("submit.html")
+	return render_template("submit.html", t1=total1, t2=total2, t3=total3)
