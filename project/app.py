bc6c5280ccc3debb6b01375428df10fdd5aac6d0
Karan <karanbhatia@protonmail.com>
2022-03-25 15:46:44 +0530

    Data displaying unformatted in Submit Page

--- project/app.py
+++ project/app.py
@@ -1,53 +1,31 @@
-from flask import Flask, render_template
-from flask import request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_wtf import FlaskForm
+
+from werkzeug.datastructures import ImmutableMultiDict
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-	return render_template('index.html')
+	if request.method == 'GET':
+		return render_template('index.html')
 
-@app.route("/submit", methods=["GET","POST"])
+@app.route('/submit', methods=['GET','POST'])
 def submit():
 	if request.method == 'POST':
-		req = request.form
-		print(request.form.to_dict())
-		scorecomponent = ['tr','award', 'milestone', 'gameboard', 'cards']
-		total = []
-		players = ['aashish','karan','mohit']
+		form =request.form
+		formdict =  form.to_dict(flat=False)
+		formdict2 = dict((k, [(int(str(s))) for s in v][0]) for k,v in formdict.items())
 
-		#{'tr1': '1', 'tr2': '1', 'tr3': '1', 'award1': '1', 'award2': '1', 'award3': '1', 'milestone1': '1', 'milestone2': '1', 'milestone3': '1', 'gameboard1': '1', 'gameboard2': '1', 'gameboard3': '1', 'cards1': '1', 'cards2': '1', 'cards3': '1'}
-
-		for player in players:
-			for i in range(1,4):
-				for component in scorecomponent:
-					key=component+str(i)
-					try:
-						playername = player
-						if() 
+		#print(formdict2[tr1])
+		formdict2['total1'] = int(request.form.get("tr1"))+int(request.form.get("award1"))+int(request.form.get("milestone1"))+int(request.form.get("gameboard1"))+int(request.form.get("cards1"))
+		formdict2['total2'] = int(request.form.get("tr2"))+int(request.form.get("award2"))+int(request.form.get("milestone2"))+int(request.form.get("gameboard2"))+int(request.form.get("cards2"))
+		formdict2['total3'] = int(request.form.get("tr3"))+int(request.form.get("award3"))+int(request.form.get("milestone3"))+int(request.form.get("gameboard3"))+int(request.form.get("cards3"))
+		
+		print(formdict2)
+		return render_template("submit.html", form=formdict2)
 
 
-		total1 = int(req.get("tr1"))+int(req.get("award1"))+int(req.get("milestone1"))+int(req.get("gameboard1"))+int(req.get("cards1"))
-		total2 = int(req.get("tr2"))+int(req.get("award2"))+int(req.get("milestone2"))+int(req.get("gameboard2"))+int(req.get("cards2"))
-		total3 = int(req.get("tr3"))+int(req.get("award3"))+int(req.get("milestone3"))+int(req.get("gameboard3"))+int(req.get("cards3"))
+					
 
-      # try:
-      #    nm = request.form['nm']
-      #    addr = request.form['add']
-      #    city = request.form['city']
-      #    pin = request.form['pin']
-         
-      #    with sql.connect("database.db") as con:
-      #       cur = con.cursor()
-      #       cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
-            
-      #       con.commit()
-      #       msg = "Record successfully added"
-      # except:
-      #    con.rollback()
-      #    msg = "error in insert operation"
-      
-      # finally:
-      #    return render_template("result.html",msg = msg)
-      #    con.close()
-	return render_template("submit.html", t1=total1, t2=total2, t3=total3)
